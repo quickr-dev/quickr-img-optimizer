f2ab903ca9709d4f2441541f8045c814a10d42b2
Rafael Garcia <rafbgarcia@gmail.com>
2024-09-29 16:35:26 -0300

    Working component

--- site/app/root.tsx
+++ site/app/root.tsx
@@ -1,27 +1,15 @@
-import type { LinksFunction } from "@remix-run/cloudflare"
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react"
-
-import "@radix-ui/themes/styles.css"
 import "./tailwind.css"
 
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-]
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react"
+
+// export const links: LinksFunction = () => [
+//   { rel: "preconnect", href: "https://fonts.googleapis.com" },
+//   { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
+//   {
+//     rel: "stylesheet",
+//     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+//   },
+// ]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -32,6 +20,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
+
       <body>
         {children}
         <ScrollRestoration />
